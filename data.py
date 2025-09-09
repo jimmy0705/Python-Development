ef6f7998a908d8a2393d8e502076d2463d8fc7d8
jimmy0705 <junedsarwar@email.com>
2025-09-09 17:08:04 +0530

    understanding data types in pyython and how to run python code

---
+++ data.py
@@ -0,0 +1,13 @@
+list1  = ["hello world", "apple", 123, 3.5, "yoo"]
+# print(list1)
+for list in list1:
+    print(list)
+
+
+dict = {
+    "fruit": "apple",
+    "id": 12345,
+
+}
+
+print(dict['id'])
