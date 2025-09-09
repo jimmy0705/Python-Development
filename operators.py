79d128ef8129151c80cf340e531618565e0fae3e
jimmy0705 <junedsarwar@email.com>
2025-09-09 18:07:15 +0530

    understood operators

---
+++ operators.py
@@ -0,0 +1,20 @@
+
+# Arithmetic Operations
+
+a = 5
+b =3
+c =19.5
+d = 31
+e = 9
+
+print(e % a)
+# print(5/3)
+print(2**4)
+#floor division
+print(5//3)
+
+# Assignment Operations
+
+x = 5
+x += 3
+print(x)
